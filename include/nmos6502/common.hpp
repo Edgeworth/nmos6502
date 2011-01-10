55ad846bee0eb0a77dca195ae743ac703b589d58
E <edgeworth.dev@gmail.com>
2011-01-10 09:43:39 +0800

    Implemented various functions for the Memory class.

--- include/nmos6502/common.hpp
+++ include/nmos6502/common.hpp
@@ -9,6 +9,31 @@ namespace nmos6502 {
 	typedef boost::uint16_t uint16;
 	typedef boost::int16_t int16;
 
+
+	/** \brief Set bit in position \a p
+	 *
+	 * \param x Integer to set bit in
+	 * \param p Position of bit to be set (starting from 0)
+	 */
+	template<typename T>
+	inline void set(T& x, int p) {x |= 1<<p;}
+
+	/** \brief Unset bit in position \a p
+	 *
+	 * \param x Integer to unset bit in
+	 * \param p Position of bit to be unset (starting from 0)
+	 */
+	template<typename T>
+	inline void unset(T& x, int p) {x &= ~(1<<p);}
+
+	/** \brief Get bit in position \a p
+	 *
+	 * \param x Integer to get bit in
+	 * \param p Position of bit to get (starting from 0)
+	 */
+	template<typename T>
+	inline bool get(T x, int p) {return x&(1<<p);}
+
 }
 
 #endif
--- src/nmos6502/memory.cpp
+++ src/nmos6502/memory.cpp
@@ -1,1 +1,51 @@
 #include "nmos6502/memory.hpp"
+
+namespace nmos6502 {
+	Memory::Memory() {
+		for (int i = 0; i < (1<<16); ++i) {
+			m[i] = 0;
+			r_cb[i] = NULL;
+			r_cb_udata[i] = NULL;
+			w_cb[i] = NULL;
+			w_cb_udata[i] = NULL;
+		}
+	}
+
+	uint8 Memory::r8(uint16 addr) {
+		if (r_cb[addr] == NULL) return m[addr];
+		return r_cb[addr](m, addr, r_cb_udata[addr]);
+	}
+
+	void Memory::w8(uint16 addr, uint8 data) {
+		if (w_cb[addr] == NULL) {
+			m[addr] = data;
+			return;
+		}
+		w_cb[addr](m, addr, data, w_cb_udata[addr]);
+	}
+
+	void Memory::setReadCB(uint16 start, uint16 end, readCB cb, void* userdata) {
+		for (int i = start; i <= end; ++i) {
+			r_cb[i] = cb;
+			r_cb_udata[i] = userdata;
+		}
+	}
+
+	void Memory::setReadCB(uint16 addr, readCB cb, void* userdata) {
+		r_cb[addr] = cb;
+		r_cb_udata[addr] = userdata;
+	}
+
+	void Memory::setWriteCB(uint16 start, uint16 end, writeCB cb, void* userdata) {
+		for (int i = start; i <= end; ++i) {
+			w_cb[i] = cb;
+			w_cb_udata[i] = userdata;
+		}
+	}
+
+	void Memory::setWriteCB(uint16 addr, writeCB cb, void* userdata) {
+		w_cb[addr] = cb;
+		w_cb_udata[addr] = userdata;
+	}
+
+}
